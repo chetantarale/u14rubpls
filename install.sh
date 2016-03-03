ca07fc3d2ad917c4fbc9501ba29be110c048d269
chetantarale <chetan@shippable.com>
2016-03-03 16:58:51 +0530

    adds libgmp3-dev package

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ sudo apt-get clean
 sudo apt-get update
 sudo apt-get install libgmp3-dev
 
+
 echo "================= Updating global rvm packages ==================="
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
